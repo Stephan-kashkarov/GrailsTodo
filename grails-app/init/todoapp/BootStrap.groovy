b32856f719f16d06d71b260a86c42aad7ec9d2d4
Stephan Kashkarov <KashkarovWork@gmail.com>
2020-07-06 20:41:39 +1000

    added bootstrapper

--- grails-app/init/todoapp/BootStrap.groovy
+++ grails-app/init/todoapp/BootStrap.groovy
@@ -3,6 +3,11 @@ package todoapp
 class BootStrap {
 
     def init = { servletContext ->
+        UserService userService
+        ItemService itemService
+        ListService listService
+        ListUserService listUserService
+        ItemListService itemListService
     }
     def destroy = {
     }
